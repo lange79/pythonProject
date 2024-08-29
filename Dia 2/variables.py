78580e79e3f2010727158c78f0a90020ba418e91
Diego Esposito <desposito@teco.com.ar>
2024-08-29 18:13:46 -0300

    dia 2

---
+++ Dia 2/variables.py
@@ -0,0 +1,18 @@
+nombre ="Juan"
+print (nombre)
+nombre ="Laura"
+print (nombre)
+edad= 30
+print(edad)
+edad2=15
+print(edad+edad2)
+nombre=input("Dime tu Nombre:")
+print("Tu nombre es:", nombre)
+
+palabra1="Hola"
+palabra2="Python"
+frase = palabra1 + palabra2
+print (frase)
+
+
+

a066f8845f71898a7dbf6bfa36117ff448855837
Diego Esposito <desposito@teco.com.ar>
2024-09-04 05:00:41 -0300

    Commit dia 3

---
+++ Dia 3/index.py
@@ -0,0 +1,12 @@
+#mi_texto = "esta es una prueba"
+#resultado = mi_texto[-4]
+#print(resultado)
+
+
+#mi_texto = "esta es una prueba"
+#resultado = mi_texto.index("a", 5, 11)
+#print(resultado)
+
+mi_texto = "esta es una prueba"
+resultado = mi_texto.rindex("a")
+print(resultado)

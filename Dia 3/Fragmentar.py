4469112e897a06d535b104a32fd19ec5d1feaade
Diego Esposito <desposito@teco.com.ar>
2024-09-13 02:50:43 -0300

    Commit dia 3

---
+++ Dia 3/Fragmentar.py
@@ -0,0 +1,15 @@
+texto= "ABCDEFGHIJKLM"
+fragmento = texto[2:5]
+print(fragmento)
+
+texto= "ABCDEFGHIJKLM"
+fragmento = texto[2:10:2]
+print(fragmento)
+
+frase = "Controlar la complejidad es la esencia de la programación"
+slicing = frase[0:9]
+print (slicing)
+
+frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+slicing=frase[9::3]
+print(slicing)
--- Dia 3/index.py
+++ Dia 3/index.py
@@ -10,3 +10,11 @@
 mi_texto = "esta es una prueba"
 resultado = mi_texto.rindex("a")
 print(resultado)
+
+frase = "Controlar la complejidad es la esencia de la programación"
+slicing = frase[0:9]
+print (slicing)
+
+frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+slicing=frase[9::3]
+print(slicing)

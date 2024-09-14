3b411dc624a89baa020aa55119bf80d7dec3475c
Diego Esposito <desposito@teco.com.ar>
2024-09-14 00:34:00 -0300

    Commit dia 3

---
+++ Dia 3/Propiedades_Strings.py
@@ -0,0 +1,20 @@
+nombre ="Carina"
+#nombre[0] = "K"
+#print(nombre)
+nombre= "Karina"
+print(nombre)
+n1="Kari"
+n2="na"
+print(n1+n2)
+print(n1*5)
+
+poema = ("""Mil pequeños peces blancos
+como si hirviera 
+el color del agua""")
+print(poema)
+
+print("agua" in poema)
+print("agua" not in poema)
+print("sol" not in poema)
+
+print(len(poema))

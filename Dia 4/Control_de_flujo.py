0453db4df3f33ad28b6e2cb5effa8a7e9e73f279
Diego Esposito <desposito@teco.com.ar>
2024-09-19 00:53:17 -0300

    Commit dia 3

---
+++ Dia 4/Control_de_flujo.py
@@ -0,0 +1,29 @@
+if 10 > 9:
+    print("Es correcto")
+else:
+    print("Es incorrecto")
+
+
+mascota="perro"
+if mascota =="gato":
+    print("tienes un gato")
+elif mascota == "perro":
+    print("Tienes un perro")
+elif mascota =="pez":
+    print("tienes un pez")
+else:
+    print("no se que animal tienes")
+
+
+edad=16
+calificacion=9
+
+if edad <18:
+    print("eres menor de edad")
+    if calificacion>=7:
+        print("aprobado")
+    else:
+        print("no aprobado")
+else:
+    print("eres adulto")
+

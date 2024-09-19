4ca624bb8a54166c0b48a3db9afd1b79ae0c6447
Diego Esposito <desposito@teco.com.ar>
2024-09-18 23:41:13 -0300

    Commit dia 3

---
+++ Dia 4/Operadores_de_Comparacion.py
@@ -0,0 +1,16 @@
+mi_variable = "hola mundo"
+mi_bool= 10==25
+print(mi_bool)
+
+mi_bool = 5+5 == 18-8
+print(mi_bool)
+
+mi_bool = "blanco"=="negro"
+print(mi_bool)
+
+mi_bool = "blanco"=="Blanco".lower()
+print(mi_bool)
+
+mi_bool =5>==8
+print(mi_bool)
+

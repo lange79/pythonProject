6fb3ae769828910fbb1f178567a573b7ac499479
Diego Esposito <desposito@teco.com.ar>
2024-10-17 00:48:41 -0300

    Commit dia 4

---
+++ Dia 4/rango.py
@@ -0,0 +1,17 @@
+lista=[1,2,3,4]
+
+for numero in lista:
+    print(numero)
+
+for numero in range(5):
+    print(numero)
+
+for numero in range(20,31):
+    print(numero)
+
+for numero in range(30,41,3):
+    print(numero)
+
+
+lista = list(range(1,101))
+print(lista)

16179af26f917ae1284c55788d4c44dfe7cb05f7
Diego Esposito <desposito@teco.com.ar>
2024-10-17 19:09:20 -0300

    Commit dia 4

---
+++ Dia 4/min_max.py
@@ -0,0 +1,24 @@
+menor = min(58,96,72,64,35)
+mayor= max(58,96,72,64,35)
+print(menor,mayor)
+
+lista= [58,96,72,64,35]
+
+print(f"El menor es {min(lista)} y el maximo es {max(lista)}")
+
+nombres = ['Juan','Pablo','Alicia','Carlos']
+print(min(nombres))
+
+
+nombre="Carlos"
+print(min(nombre))
+
+nombre="Carlos"
+print(min(nombre.lower()))
+
+
+dic={'C1':45,'C2':11}
+print(min(dic))
+
+dic={'C1':45,'C2':11}
+print(min(dic.values()))

2836b8cbeccf8d0c4bb263911f0f43299189bb8d
Diego Esposito <desposito@teco.com.ar>
2024-10-16 22:50:15 -0300

    Commit dia 4

--- Dia 4/loop_For.py
+++ Dia 4/loop_For.py
@@ -66,3 +66,22 @@ dic = {'clave1':'a', 'clave2':'b','clave3':'c'}
 for a,b in dic.items():
     print(a,b)
 
+alumnos_clase = ["María", "José", "Carlos", "Martina", "Isabel", "Tomás", "Daniela"]
+for alumno in alumnos_clase:
+    print(f"Hola {alumno}")
+
+
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+suma_numeros = 0
+for numero in lista_numeros:
+    suma_numeros = suma_numeros+numero
+
+
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+suma_pares = 0
+suma_impares = 0
+for numero in lista_numeros:
+    if (numero%2==0):
+        suma_pares=suma_pares+numero
+    else:
+        suma_impares=suma_impares+numero

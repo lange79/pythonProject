c921ff38d7e33b404542feeac43a1720cdc890b0
Diego Esposito <desposito@teco.com.ar>
2024-10-22 17:43:50 -0300

    Commit dia 5

---
+++ Dia 5/Crear_funciones.py
@@ -0,0 +1,14 @@
+def saludar_persona(nombre):
+    """
+    ESta funcion sirve para saludar a las personas
+    """
+    print("Hola "+nombre)
+
+saludar_persona("Diego")
+saludar_persona("Javier")
+
+
+def cuadrado(un_numero):
+    print(un_numero**2)
+
+un_numero=10

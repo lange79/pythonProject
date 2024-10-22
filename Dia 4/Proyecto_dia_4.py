a9475dd5d144fdc36f95ee88dfed131da11475b2
Diego Esposito <desposito@teco.com.ar>
2024-10-22 14:23:16 -0300

    Commit dia 4

---
+++ Dia 4/Proyecto_dia_4.py
@@ -0,0 +1,27 @@
+jugador=input("Ingrese su nombre:")
+print(f"Hola {jugador}, he pensado un numero entre 1 y 100, tienes 8 intentos para adivinar cual crees que es el numero")
+from random import randint
+numero=randint(1,100)
+intentos=1
+while intentos in range(1,9):
+    numero_Jugador = int(input("Ingrese el numero que cree que he pensado: "))
+    if 0<numero_Jugador<=100:
+        if numero_Jugador==numero:
+            print(f"Felicidades {jugador} has adivinado el numero en {intentos} intentos!!")
+            break
+        elif numero_Jugador<numero:
+            print(f"Incorrecto {jugador}!. El numero elegido es menor al numero que he pensado."
+                  f"Te quedan {8-intentos} intentos")
+        else:
+            print(f"Incorrecto {jugador}!. El numero elegido es mayor al numero que he pensado."
+                  f"Te quedan {8-intentos} intentos")
+    else:
+        print(f"Lo siento {jugador}, has elegido un numero que no esta permitido.")
+        continue
+    intentos+=1
+
+
+
+
+
+

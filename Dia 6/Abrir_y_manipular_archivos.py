0c0ac035c9f63eabf118c5fc51f001552118a159
Diego Esposito <desposito@teco.com.ar>
2024-11-04 20:44:49 -0300

    Commit dia 5

---
+++ Dia 6/Abrir_y_manipular_archivos.py
@@ -0,0 +1,35 @@
+mi_archivo = open('archivo.txt')
+print (mi_archivo)
+print(mi_archivo.read())
+
+
+mi_archivo = open('Prueba.txt')
+
+print(mi_archivo.readline())
+
+una_linea=mi_archivo.readline()
+print(una_linea.rstrip())
+
+una_linea=mi_archivo.readline()
+print(una_linea.upper())
+
+mi_archivo = open('Prueba.txt')
+
+todas=mi_archivo.readlines()
+print(todas)
+
+
+
+mi_archivo = open('Prueba.txt')
+
+
+for l in mi_archivo:
+    print ("Aqui dice: " + l)
+
+mi_archivo = open('Prueba.txt')
+todas=mi_archivo.readlines()
+todas=todas.pop()
+print (todas)
+
+
+mi_archivo.close()

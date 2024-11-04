24cedccf618fdd7059fcc06c1d73fc475cdad46e
Diego Esposito <desposito@teco.com.ar>
2024-11-04 20:02:52 -0300

    Commit dia 5

--- Dia 5/Ayuda.py
+++ Dia 5/Ayuda.py
@@ -3,3 +3,20 @@ dic = {'clave1':100,'clave2':500}
 a = dic.popitem()
 print(a)
 print(dic)
+
+dic.popitem()
+
+
+x = ",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#"
+out = x.lstrip(",:%_#")
+print(out)
+
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+frutas.insert(3, "naranja")
+print(frutas)
+
+
+marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
+conjuntos_aislados=marcas_smartphones.isdisjoint(marcas_tv)
+print(conjuntos_aislados)

25b401396fda3dd684c749a708abda1d26958f67
Diego Esposito <desposito@teco.com.ar>
2024-11-22 03:26:01 -0300

    Dia 8

---
+++ Dia 8/turnos_main.py
@@ -0,0 +1,31 @@
+import turnos
+
+def preguntar():
+    print("Bienvenido a Famacia Python")
+
+    while True:
+        print("[P] - Perfumeria\n[F] - Farmacia\n[C] - Cosmetica")
+        try:
+            mi_rubro = input("Elija su rubro: ").upper()
+            ["P","F","C"].index(mi_rubro)
+        except ValueError:
+            print("Esa no es una opcion valida.")
+        else:
+            break
+
+    turnos.decorador(mi_rubro)
+
+def inicio():
+    while True:
+        preguntar()
+        try:
+            otro_turno = input("Quieres sacar otro turno? [S]/[N]").upper()
+            ["S","N"].index(otro_turno)
+        except ValueError:
+            print("Esa no es una opcion valida")
+        else:
+            if otro_turno == "N":
+                print("Gracias por su visita")
+                break
+
+inicio()

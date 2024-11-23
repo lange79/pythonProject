f2167f2aa71a152ad953f65bf2d9d229561bec8d
Diego Esposito <desposito@teco.com.ar>
2024-11-23 12:27:41 -0300

    Dia 8

---
+++ Dia 9/fecha_hora.py
@@ -0,0 +1,38 @@
+import datetime
+from calendar import month
+
+mi_hora= datetime.time(17,35)
+mi_dia=datetime.date(2024,11,23)
+
+
+print(mi_hora)
+print(mi_dia)
+print(mi_dia.ctime())
+print(mi_dia.today())
+
+
+from datetime import datetime
+mi_fecha = datetime(2025,5,15,22,10,15,2500)
+mi_fecha = mi_fecha.replace(month = 7)
+
+print(mi_fecha)
+
+
+from datetime import date
+
+nacimiento=date(1995,3,5)
+defuncion=date(2095,6,19)
+
+vida= defuncion-nacimiento
+
+print("Vivio: ",vida)
+print(vida.days)
+
+from datetime import datetime
+
+despierta = datetime(2022,10,5,7,30)
+duerme= datetime(2022,10,5,23,45)
+
+vigilia=duerme-despierta
+print(vigilia)
+print(vigilia.seconds)

c1ae0caeb9e3a67976eb885b0676834a931c26cf
Diego Esposito <desposito@teco.com.ar>
2024-11-26 01:13:21 -0300

    Dia 9

---
+++ Dia 9/modulo_math.py
@@ -0,0 +1,18 @@
+import math
+resultado = math.floor(89.655)
+print(resultado)
+
+resultado = math.ceil(89.655)
+print(resultado)
+
+resultado = math.pi
+print(resultado)
+
+resultado = math.log(100,10)
+print(resultado)
+
+resultado = math.sin(0)
+print(resultado)
+
+resultado = math.cos(0)
+print(resultado)
